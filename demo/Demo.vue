b78d67fc099b1984aca279d7020bad501af7825c
chenfeng <chenyuhu428@gmail.com>
2019-12-12 11:47:48 +0800

    修改文件格式以及导出时报错

---
+++ demo/Demo.vue
@@ -0,0 +1,20 @@
+<!-- demo -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  props: {},
+  methods: {},
+  created() {},
+  components: {},
+  watch: {},
+  computed: {},
+}
+</script>
+
+<style scoped lang="scss"></style>
