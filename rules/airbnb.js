b97f27150cb077e8d60392e594460cce34611eb1
杨俊宁 <youngjuning@aliyun.com>
2019-12-13 14:04:40 +0800

    禁止对函数参数再赋值

--- rules/airbnb.js
+++ rules/airbnb.js
@@ -11,9 +11,9 @@ module.exports = {
   'max-classes-per-file': 0,
   'no-console': 0,
   'no-nested-ternary': 1, // 禁止使用嵌套的三元表达式
-  'no-param-reassign': [2, { props: false }],
+  'no-param-reassign': [2, { props: false }], // 禁止对函数参数再赋值
   'no-restricted-globals': 0, // 禁用特定的全局变量
-  'no-use-before-define': [2, { functions: true, classes: true, variables: false }], // 禁止定义前使用（变量例外）
+  'no-use-before-define': 0, // 禁止定义前使用
   'no-underscore-dangle': [2, { allow: ['__DEV__'] }], // 禁止标识符中有悬空下划线
   'no-unused-expressions': 0, // 禁止未使用过的表达式
   'no-unused-vars': 0,
