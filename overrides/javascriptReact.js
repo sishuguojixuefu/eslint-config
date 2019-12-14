840fe3c275f2c8b7cb276d89fcc436ed612ad6ca
杨俊宁 <youngjuning@aliyun.com>
2019-12-14 17:56:30 +0800

    Update javascriptReact.js

--- overrides/javascriptReact.js
+++ overrides/javascriptReact.js
@@ -4,9 +4,6 @@ const reactRules = require('../rules/react.js')
 
 module.exports = {
   files: ['*.jsx'],
-  env: {
-    'react-native/react-native': true,
-  },
   extends: ['airbnb', 'prettier', 'prettier/react'],
   parser: 'babel-eslint',
   parserOptions: {
