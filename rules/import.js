44de33687690d0951d969c43d587b4800ca36667
杨俊宁 <youngjuning@aliyun.com>
2019-12-21 11:14:58 +0800

    add import/prefer-default-export: 0

--- rules/import.js
+++ rules/import.js
@@ -1,4 +1,5 @@
 module.exports = {
+  'import/prefer-default-export': 0, // When there is only a single export from a module, prefer using default export over named export.
   'import/extensions': 0, // Ensure consistent use of file extension within the import path
   'import/no-extraneous-dependencies': 0, // Forbid the use of extraneous packages
 }
