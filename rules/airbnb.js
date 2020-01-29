78e105910dbbffa8e32f8fc59c0a67d5644565e5
杨俊宁 <youngjuning@aliyun.com>
2020-01-29 21:32:18 +0800

    'class-methods-use-this': 0,

--- rules/airbnb.js
+++ rules/airbnb.js
@@ -10,6 +10,7 @@ module.exports = {
   ],
   'no-useless-constructor': 1,
   'max-classes-per-file': 0,
+  'class-methods-use-this': 0,
   'no-console': 0,
   'no-plusplus': 0,
   'no-return-assign': 0,
