65f64f2abfb732f63995c5b7e22d7e15a4daa587
杨俊宁 <youngjuning@aliyun.com>
2020-02-05 13:22:36 +0800

    add eslint-plugin-you-dont-need-momentjs

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ module.exports = {
     'plugin:jest/recommended',
     'plugin:prettier/recommended',
     'plugin:you-dont-need-lodash-underscore/compatible',
+    'plugin:you-dont-need-momentjs/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
