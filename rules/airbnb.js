9200e1ad195178e9dba8a95cf5caf3a838c39acc
杨俊宁 <youngjuning@aliyun.com>
2020-03-16 18:26:47 +0800

    'consistent-return': 0,

--- rules/airbnb.js
+++ rules/airbnb.js
@@ -12,6 +12,7 @@ module.exports = {
   'max-classes-per-file': 0,
   'class-methods-use-this': 0,
   'import/no-cycle': 0,
+  'consistent-return': 0,
   'no-console': 0,
   'no-plusplus': 0,
   'no-return-assign': 0,
